edea899d0f993537373613408bb848c037d6e434
Khalilov Nariman <76436187+Nariman20066254@users.noreply.github.com>
2025-12-27 11:51:46 +0500

    Add files via upload

---
+++ Lesson_3/main.py
@@ -0,0 +1,38 @@
+from silero_tts.silero_tts import SileroTTS
+import pygame 
+import os
+
+
+def say(text):
+    name_waw = 'output.wav' 
+
+    tts = SileroTTS(
+        language='ru',
+        model_id='v4_ru',
+        speaker='baya'
+    )
+
+    tts.tts(text, f'{name_waw}')
+    print(f"{text}✅")
+    muc(f'{name_waw}')
+
+def muc(file_name):
+    """
+    Проигрывает аудиофайл и удаляет его после завершения.
+    """
+    try:
+        pygame.mixer.init()
+        pygame.mixer.music.load(file_name)
+        pygame.mixer.music.play()
+        while pygame.mixer.music.get_busy():
+            pygame.time.Clock().tick(10)  # уменьшает нагрузку на CPU
+
+        pygame.mixer.music.stop()        # ⛔ Остановить
+        pygame.mixer.quit()              # 🧹 Освободить ресурс
+
+        os.remove(file_name)             # ✅ Удалить после выгрузки
+    except Exception as e:
+        print(f'Ошибка в функции play_music: {e}')
+
+
+say('Привет меня зовут нариман ')
